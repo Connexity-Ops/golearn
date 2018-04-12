d85f0095cc1b07f9fcd3bf29e3d1ab8707342ae2
Peter Auyeung <161390@szpauyeung-m.local>
2018-04-11 20:08:53 -0700

    failing at empty int array

--- sdd/simple-cipher/simpleCipher.go
+++ sdd/simple-cipher/simpleCipher.go
@@ -1,4 +1,4 @@
-package simpleCipher
+package simplecipher
 
 // simpleEncrypt takes a string like "Hello World" and returns an array of integers that are the
 // numeric equivalent of the letters. Example A=1, B=2, C=3, ...
@@ -6,6 +6,23 @@ package simpleCipher
 // for example "2" will return 32.
 // If the character is a space then the return code is a 0.
 // All punctuation and special characters are ignored.
+
 func simpleEncrpyt(r string) []int {
 	// TODO return an array of integer(s) mapping to string.
+	var ret []int
+	for i := 0; i < len(r); i++ {
+		c := int(r[i])
+
+		switch {
+		case c > 96 && c < 123:
+			ret = append(ret, c-96)
+		case c > 64 && c < 91:
+			ret = append(ret, c-64)
+		case c > 47 && c < 58:
+			ret = append(ret, c-47+29)
+		case c == 32:
+			ret = append(ret, c-32)
+		}
+	}
+	return (ret)
 }
