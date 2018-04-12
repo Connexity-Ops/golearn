cb17a5333ea059d3f7347f01b58e85ec629e9946
vonkimber <vonkimber@gmail.com>
2018-04-12 00:21:40 -0700

    cleaning up comments and removing imports

--- sdd/mapsort/mapsort.go
+++ sdd/mapsort/mapsort.go
@@ -1,10 +1,5 @@
 package mapsort
 
-import (
-//	"fmt"
-//	"sort"
-)
-
 // Pair creates an array to hold Key/Value pairs
 type Pair struct {
 	Key   string
@@ -14,7 +9,7 @@ type Pair struct {
 // PairList initializes a slice of array Pair
 type PairList []Pair
 
-// MapSort takes a map and sorts it alphabetically by Key into a slice of Pair
+// MapSort sorts a map's Keys alphabetically by unique Value, and returns it as an ordered slice of []Pair
 func MapSort(m map[string]int) PairList {
 
 	//YOUR CODE HERE
