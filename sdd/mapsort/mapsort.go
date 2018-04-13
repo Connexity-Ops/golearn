a880714fa1a2121678d675ecb4223f09e621ba09
Mike Jones <mjones@shopzilla.com>
2018-04-13 09:50:01 -0600

    Added mapsort function that satifies test

--- sdd/mapsort/mapsort.go
+++ sdd/mapsort/mapsort.go
@@ -1,17 +1,26 @@
 package mapsort
 
-// Pair creates an array to hold Key/Value pairs
+import (
+	"sort"
+)
+
+// Pair creates a struct to hold Key/Value pairs
 type Pair struct {
 	Key   string
 	Value int
 }
 
-// PairList initializes a slice of array Pair
+// PairList initializes a slice of Pair structs
 type PairList []Pair
 
-// MapSort sorts a map's Keys alphabetically by unique Value, and returns it as an ordered slice of []Pair
+// MapSort sorts a map's Keys by Value, and returns it as an ordered slice of []Pair
 func MapSort(m map[string]int) PairList {
-
-	//YOUR CODE HERE
-
+	var kvPairs PairList
+	for k, v := range m {
+		kvPairs = append(kvPairs, Pair{Key: k, Value: v})
+	}
+	sort.Slice(kvPairs, func(i, j int) bool {
+		return kvPairs[i].Value < kvPairs[j].Value
+	})
+	return kvPairs
 }
