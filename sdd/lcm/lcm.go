7a1f1987fb39765e5f63b8700e55fdd919b36732
Jordan Smith <jsmith@connexity.com>
2018-04-13 19:26:26 -0700

    added LcmCalc function and changed test to require int64

--- sdd/lcm/lcm.go
+++ sdd/lcm/lcm.go
@@ -1,7 +1,26 @@
 package lcm_calc
 
-//LcmCalc Find the Lowest Common Multiple of a list of integers
-func LcmCalc(numbers []int) (lcm int, err error) {
-	// your code here
-	return lcm, err
+import (
+	"math"
+)
+
+// gcd finds the greatest common denominator of two numbers.
+func gcd(x, y int64) int64 {
+
+	for y != 0 {
+		x, y = y, x%y
+	}
+
+	return x
+}
+
+// LcmCalc finds the Least Common Multiple using the gcd function on an array of numbers.
+func LcmCalc(numbers []int64) int64 {
+	x := numbers[0]
+	for _, y := range numbers {
+		x = x * y / gcd(x, y)
+	}
+
+	return int64(math.Abs(float64(x)))
+
 }
--- sdd/lcm/lcm_test.go
+++ sdd/lcm/lcm_test.go
@@ -8,37 +8,37 @@ func TestLcmCalc(t *testing.T) {
 
 	tests := []struct {
 		name  string
-		args  []int
-		wants int
+		args  []int64
+		wants int64
 	}{
 		{
 			name:  "Two numbers",
-			args:  []int{3, 5},
+			args:  []int64{3, 5},
 			wants: 15,
 		},
 		{
 			name:  "Three numbers",
-			args:  []int{3, 5, 9},
+			args:  []int64{3, 5, 9},
 			wants: 45,
 		},
 		{
 			name:  "Large numbers",
-			args:  []int{38756, 5487, 93294},
+			args:  []int64{38756, 5487, 93294},
 			wants: 3306559720428,
 		},
 		{
 			name:  "Larger array",
-			args:  []int{5, 17, 90, 23, 23, 43, 86, 75, 109, 203},
+			args:  []int64{5, 17, 90, 23, 23, 43, 86, 75, 109, 203},
 			wants: 167409562950,
 		},
 		{
 			name:  "Numbers with zero",
-			args:  []int{45, 0, 5, 9},
+			args:  []int64{45, 0, 5, 9},
 			wants: 0,
 		},
 		{
 			name:  "Negative Numbers",
-			args:  []int{-8, 2, -5, 4},
+			args:  []int64{-8, 2, -5, 4},
 			wants: 40,
 		},
 	}
